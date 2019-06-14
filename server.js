68be73bc8bb9ed72c84a9738472352060215d5b5
Ospirentos <ospirentos@gmail.com>
2019-06-15 02:44:18 +0300

    login database check added

--- server.js
+++ server.js
@@ -9,14 +9,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/api/logincheck', (req, res) => {
-  console.log(req.body);
+  console.log(req.body.email);
   const client = new MongoClient(uri, { useNewUrlParser: true });
   client.connect(err => {
     if (err) throw err;
     const collection = client.db("reactnative").collection("users");
-        collection.find({name:"ospirentos"}).toArray(function(err, result) {
+        collection.find({email:req.body.email}).toArray(function(err, result) {
         if (err) throw err;
-        res.send("true")
+        if (result !== 'undefined' && result.length > 0) {
+          if(req.body.password === result[0].password) {
+            res.send("true")
+          } else {
+            res.send("false")
+          }
+        } else {
+          res.send("false")
+        }
       })
   
     client.close();
